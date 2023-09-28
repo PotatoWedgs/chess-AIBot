e2ce1329b1194a9a53c186e4c1f12abedfb29569
Abdullah Khadeli <akhadeli@ualberta.ca>
2023-09-28 06:26:07 +0300

    new commit

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import torch
 import pandas as pd
 from torch import nn
 from torch.optim import Adam
-from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
 import torch
 import numpy as np
+from pathlib import Path
+from functools import lru_cache
 
-#   Our Iris dataset
+"""#   Our Iris dataset
 data = pd.read_csv('chess_data/csv/game1.csv')
 
 #   Setuping out X and y data
@@ -25,9 +27,57 @@ X_train = torch.FloatTensor(X_train)
 X_test = torch.FloatTensor(X_test)
 
 y_train = torch.LongTensor(y_train)
-y_test = torch.LongTensor(y_test)
+y_test = torch.LongTensor(y_test)"""
+
+@lru_cache()
+def get_sample_count_by_file(path: Path) -> int:
+    c = 0
+    with path.open() as f:
+        for line in f:
+            c += 1
+    return c
 
 
+class CSVDataset:
+    def __init__(self, csv_directory: str, extension: str = ".csv"):
+        self.directory = Path(csv_directory)
+        self.files = sorted((f, get_sample_count_by_file(f)) for f in self.directory.iterdir() if f.suffix == extension)
+        self._sample_count = sum(f[-1] for f in self.files)
+
+    def __len__(self):
+        return self._sample_count
+
+    def __getitem__(self, idx):
+        current_count = 0
+
+        history_window = 2
+        my_idx=idx+2
+
+        for file_, sample_count in self.files:
+            if current_count <= my_idx < current_count + sample_count:
+                break  
+            current_count += sample_count
+
+        file_idx = my_idx - current_count # the index we want to access in file_
+        if file_idx < 2:
+            file_idx += 2
+
+        with file_.open() as f:
+            data = []
+            for i, line in enumerate(f):
+                if i >= file_idx-history_window and i <= file_idx:
+                    for v in line.split(","):
+                        data.append(float(v))
+
+            data = np.array(data)
+            return torch.from_numpy(data)
+
+dataset = CSVDataset("chess_data/csv")
+loader = DataLoader(dataset, batch_size=1)
+
+print(list(enumerate(loader)))
+
+"""
 # Chess Neural Network
 class ChessNeuralNetwork(nn.Module): 
     def __init__(self):
@@ -90,4 +140,4 @@ if __name__ == "__main__":
 
         print(f'We got {correct}/{len(X_test)} correct')
 
-    #torch.save(model.state_dict(), 'model.pt')
+    #torch.save(model.state_dict(), 'model.pt')"""
